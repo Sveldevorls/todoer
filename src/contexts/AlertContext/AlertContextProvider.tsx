138872ad3089e21297c280667b485a9fbd268a9a
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-07-16 00:43:14 +0800

    Fix alert display bug

--- src/contexts/AlertContext/AlertContextProvider.tsx
+++ src/contexts/AlertContext/AlertContextProvider.tsx
@@ -28,7 +28,7 @@ export default function AlertProvider({ children }: { children: ReactNode }) {
         id="alert"
         ref={alertRef}
         className="
-          w-fit pl-4 pr-2 py-2 rounded-md shadow-md
+          w-fit pl-4 pr-2 py-2 rounded-md shadow-md pointer-events-none
           flex items-center gap-2
           fixed bottom-10 left-1/2 -translate-x-1/2 
         bg-gray-700 text-white 
@@ -37,7 +37,10 @@ export default function AlertProvider({ children }: { children: ReactNode }) {
         <p>{message}</p>
         <button
           className="button-svg hover:bg-gray-600 filter-none"
-          onClick={() => alertRef.current!.style.opacity = "0"}
+          onClick={() => {
+            alertRef.current!.style.opacity = "0";
+            alertRef.current!.style.pointerEvents = "none";
+          }}
         >
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round">
             <line x1="6" y1="6" x2="18" y2="18" />
